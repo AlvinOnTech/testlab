37abbc0021fae97afcfcb6f81572a5255c005982
Alvin <alvinontech@gmail.com>
2023-03-20 11:34:25 +0800

    Initial upload of scripts

---
+++ Create-CM1.ps1
@@ -0,0 +1,50 @@
+﻿#Script to create TROUGHTON
+
+#Declare Variables
+$VMStore  = 'D:\Virtual Machines'
+$VHDStore = 'D:\Virtual Hard Disks'
+$VMSwitch = "vSwitch-LAN"
+
+$VMName    = 'Lab-CM1'
+$VMRAM     = 6144MB
+$OSDrive   = 25GB
+$DataDrive = 50GB
+$ISO       = 'E:\~ISOStore\Windows\Server 2019\en_windows_server_2019_updated_sept_2019_x64_dvd_199664ce.iso'
+
+#Create Folder Structure
+$VMRoot  = "$VMStore\$VMName"
+$VHDPath = "$VHDStore\$VMName"
+If (!(Test-Path $VMRoot)) { New-Item -ItemType Directory -Path $VMRoot }
+If (!(Test-Path $VHDPath)) { New-Item -ItemType Directory -Path $VHDPath }
+
+#region Create OS Drive VHD
+If (!(Test-Path -Path "$VHDPath\OSDrive.vhdx")) {
+    Write-Host "Creating Virtual Disk $VHDPath\OSDrive.vhdx"
+    $VHD = New-VHD -Path "$VHDPath\OSDrive.vhdx" -Fixed -SizeBytes $OSDrive
+}
+Else {
+    Write-Host "Using existing Virtual Disk $VHDPath\OSDrive.vhdx"
+    $VHD = Get-VHD -Path "$VHDPath\OSDrive.vhdx"
+}
+#endregion
+
+#region Create Data Drive VHD
+If (!(Test-Path -Path "$VHDPath\DataDrive.vhdx")) {
+    Write-Host "Creating Virtual Disk $VHDPath\DataDrive1.vhdx"
+    $VHD = New-VHD -Path "$VHDPath\DataDrive.vhdx" -Fixed -SizeBytes $DataDrive
+}
+Else {
+    Write-Host "Using existing Virtual Disk $VHDPath\DataDrive.vhdx"
+    $VHD = Get-VHD -Path "$VHDPath\DataDrive.vhdx"
+}
+#endregion
+
+#Create new VM
+Write-Host "Create VM $VMName"
+$VM = New-VM -Path $VMStore -Name $VMName -MemoryStartupBytes $VMRAM -Generation 2
+Set-VM -Name $VMName -ProcessorCount 2
+Write-Host "Add VHD to VM $VMName"
+Add-VMHardDiskDrive -Path $($VHD.Path) -VM $VM -ControllerType SCSI  #Add VHDX to VM
+Remove-VMNetworkAdapter -VM $VM                                                 #Remove vNIC created with VM
+Add-VMNetworkAdapter -VM $VM -SwitchName $VMSwitch                              #Add vNIC attached to vSwitch
+Add-VMDvdDrive -VM $VM –Path $ISO -ControllerNumber 0
---
+++ Create-vSwitch.ps1
@@ -0,0 +1,15 @@
+﻿$vSwitchDef = [ordered]@{
+   Name       = 'Lab-DMZ'
+   SwitchType = 'Internal'
+   Notes      = 'DMZ for Lab - 192.168.2.0/24'
+}
+
+
+If (!(Get-VMSwitch -Name $vSwitchDef['Name'])) {
+    # If you can't find the vSwitch, create it
+    New-VMSwitch @vSwitchDef
+}
+Else {
+    # Set the new settings for the vSwitch
+    Set-VMSwitch @vSwitchDef
+}
